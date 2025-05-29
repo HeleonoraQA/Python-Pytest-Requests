a14bf49707f7c59b9343384aaab558476e6368a1
HeleonoraQA <erunova2007@mail.ru>
2025-05-29 13:35:19 +0300

    Add files via upload

--- PythonProject/RequestsTest/Tests/test_pokemon.py
+++ PythonProject/RequestsTest/Tests/test_pokemon.py
@@ -11,12 +11,11 @@ def test_status_code():
     assert response.status_code == 200
 
 def test_part_of_response():
-
     response_get = requests.get(url = f'{URL}/v2/trainers', params = {'trainer_id': TRAINER_ID})
     '''assert response_get.status_code == 200'''
     assert response_get.json()["data"][0]["trainer_name"] == 'БОБДжингер'
 
-@pytest.mark.parametrize('key, value', [('trainer_name', 'БОБДжингер'), ('id', TRAINER_ID)])
+@pytest.mark.parametrize('key, value',[(TRAINER_ID, 'БОБДжингер2'),('38558', 'Грин'),('38555', 'Бак2')])
 def test_parametrize(key,value):
-    response_parametrize = requests.get(url = f'{URL}/v2/trainers', params = {'trainer_id': TRAINER_ID})
-    assert response_parametrize.json()["data"][0][key] == value
+    response_json = requests.get(url=f'{URL}/v2/trainers', params={'trainer_id': key}).json()
+    assert response_json["data"][0]['trainer_name'] == value
